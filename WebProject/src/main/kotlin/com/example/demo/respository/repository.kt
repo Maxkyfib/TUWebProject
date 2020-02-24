4cff85fffb8ef0a7679311d557fc40d0dbfaee03
iammarkps <markkung08@gmail.com>
2020-02-24 14:07:41 +0700

    Remove backend

--- WebProject/src/main/kotlin/com/example/demo/respository/repository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo.respository
-
-import com.example.demo.entity.UserEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-
-interface repository : JpaRepository<UserEntity,String> {
-
-}
